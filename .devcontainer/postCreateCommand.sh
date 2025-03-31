eb7b2723f9bb062f5a7ebb5ca22d5ea8c97b8557
Kqramazov <yurisvarozhich@gmail.com>
2025-03-31 03:05:51 +0000

    setup_tdesktop_env

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,24 +1,2 @@
-# set npm environment
-# sudo apt install -y x11-apps curl
-# Download and install nvm:
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-# # in lieu of restarting the shell
-# \. "$HOME/.nvm/nvm.sh"
-
-# # Download and install Node.js:
-# nvm install 23
-
-# # Verify the Node.js version:
-# node -v # Should print "v23.8.0".
-# nvm current # Should print "v23.8.0".
-
-# # Verify npm version:
-# npm -v # Should print "10.9.2".
-
-
-
-# cd Mailspring
-# # export npm_config_arch=x64 # If you are on an M1 / Apple Silicon Mac
-# npm install
-# # npm start
+#!/bin/bash
+sudo chown -R agent:agent /apps/tdesktop/Debug/
