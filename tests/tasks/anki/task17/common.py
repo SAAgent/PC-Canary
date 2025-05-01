2a04f00a545863e0d8c9cc556652e3efe07a8b72
ruiqurm <ruiqurm@gmail.com>
2025-05-01 16:47:03 +0000

    tests: anki: add test17

---
+++ tests/tasks/anki/task17/common.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../common')))
+from exanki import *
+
+set_sucess_ : str = "set_sucess"  # 设置成功
+set_wrong_ : str = "set_wrong"  # 设置错误
+
+class EventSetSucess(FridaEvent):
+
+    def __init__(self,value=True):
+        super().__init__("set_sucess", value)
+
+
+    def describe(self):
+        return "设置成功"
+    
+class EventSetWrong(FridaEvent):
+
+    def __init__(self,get="",expect=""):
+        super().__init__("set_wrong", f"got {get}, expect {expect}")
+
+
+    def describe(self):
+        return "设置错误"
+    
+from dataclasses import dataclass
+@dataclass
+class TaskParameters:
+    time : str
+tp = TaskParameters(**{'time': '1'})
