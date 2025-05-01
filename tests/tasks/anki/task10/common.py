50dfad05097a1b7f3f888b2d9bf151c3201b03b3
ruiqurm <ruiqurm@gmail.com>
2025-05-01 13:53:34 +0000

    tests: anki: add test10

---
+++ tests/tasks/anki/task10/common.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../common')))
+from exanki import *
+
+clear_unused_tags_ : str = "clear_unused_tags"  # 删除所有未使用的标签
+
+class EventClearUnusedTags(FridaEvent):
+
+    def __init__(self,value=True):
+        super().__init__("clear_unused_tags", value)
+
+
+    def describe(self):
+        return "删除所有未使用的标签"
+    
