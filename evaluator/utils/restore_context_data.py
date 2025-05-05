efab808907340815f6511a7b5f189cca6ab03f18
JiajunDu <wonderboy512@163.com>
2025-05-05 15:58:12 +0800

    change the method of restoring context data to `rsync`
    Signed-off-by: JiajunDu <wonderboy512@163.com>

--- evaluator/utils/restore_context_data.py
+++ evaluator/utils/restore_context_data.py
@@ -1,9 +1,9 @@
 import os
-import shutil
+import subprocess
 
 def restore_context_data(from_path: str, to_path: str) -> bool:
     """
-    将from_path目录下的所有内容复制到to_path目录
+    将from_path目录下的所有内容增量更新到to_path目录
     
     Args:
         from_path: 源目录路径
@@ -15,26 +15,21 @@ def restore_context_data(from_path: str, to_path: str) -> bool:
     # 检查源路径是否存在
     if not os.path.exists(from_path):
         raise Exception(f"用户数据{from_path}不存在")
-
-    # 如果目标路径存在，则清理其内容
-    if os.path.exists(to_path):
-        yes_or_no = input(f"需要删除{to_path}下的所有内容:(Y or N)")
-        if yes_or_no == 'n' or yes_or_no == 'N':
-            raise Exception(f"请重新设置{to_path}的路径")
-        shutil.rmtree(to_path)
-    
     # 创建目标目录
     os.makedirs(to_path, exist_ok=True)
     
     # 复制源目录内容到目标目录
     try:
-        for item in os.listdir(from_path):
-            src_item = os.path.join(from_path, item)
-            dst_item = os.path.join(to_path, item)
-            if os.path.isdir(src_item):
-                shutil.copytree(src_item, dst_item)
-            else:
-                shutil.copy2(src_item, dst_item)
+        rsync_cmd = ["rsync", "-av", "--delete", f"{from_path}/", to_path]
+        result = subprocess.run(
+            rsync_cmd,
+            check=True,  # 如果命令失败，抛出异常
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
         return True
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"增量更新过程中发生错误: {str(e.stderr)}")
     except Exception as e:
-        raise Exception(f"复制目录过程中发生错误: {str(e)}")
+        raise Exception(f"增量更新过程中发生错误: {str(e)}")
